aa0b39d63fdf667d139f33b784a8b48224467cf8
PeraltaMartin13 <martinperaltasmurra13@gmail.com>
2026-09-08 19:05:45 -0300

    Footer terminado

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -7,36 +7,39 @@ import { Smartphone, Camera, Clapperboard, MapPin } from "lucide-react"
 export default function Footer() {
 
     return (
-        <section className=" flex w-full items-center justify-between px-30 bg-zinc-950">
-            <Image src="/media/logo-haka.png" alt="logo Haka" width={90} height={60}></Image>
-            <div className="flex flex-col p-5  text-zinc-50 ">
-                <div className="flex flex-col py-5 px-0.5">
-                    <h4 className="font-bold text-2xl">REDES SOCIALES</h4>
-                    <div className="flex gap-2 items-center">
-                        <Camera />
-                        <a href={INSTAGRAM_URL}>INSTAGRAM</a>
+        <section className=" flex flex-col w-full items-center justify-between px-30 bg-zinc-950">
+            <div className="flex w-full items-center justify-between px-30">
+                <Image src="/media/logo-haka.png" alt="logo Haka" width={150} height={60}></Image>
+                <div className="flex items-center p-5  text-zinc-50 ">
+                    <div className="flex flex-col py-5 px-10">
+                        <h4 className="font-bold text-2xl">REDES SOCIALES</h4>
+                        <div className="flex gap-2 items-center">
+                            <Camera />
+                            <a href={INSTAGRAM_URL}>INSTAGRAM</a>
+                        </div>
+                        <div className="flex gap-2 items-center">
+                            <Clapperboard />
+                            <a href={TIKTOK_URL}>TIKTOK</a>
+                        </div>
                     </div>
-                    <div className="flex gap-2 items-center">
-                        <Clapperboard />
-                        <a href={TIKTOK_URL}>TIKTOK</a>
+                    <div className="flex flex-col py-5  px-10">
+                        <h4 className="font-bold text-2xl">CONTACTO</h4>
+                        <div className="flex gap-2 items-center">
+                            <Smartphone />
+                            <p>1137727295</p>
+                        </div>
+                        <div className="flex gap-2 items-center">
+                            <MapPin />
+                            <p>Uspallata 2120, Hurlingham</p>
+                        </div>
                     </div>
+                    <a href={BOOKING_URL} className="text-zinc-100 text-xl px-10  py-4 bg-violet-400 rounded-full ">RESERVÁ TU TURNO!</a>
                 </div>
-                <div className="flex flex-col py-5  px-0.5">
-                    <h4 className="font-bold text-2xl">CONTACTO</h4>
-                    <div className="flex gap-2 items-center">
-                        <Smartphone />
-                        <p>1137727295</p>
-                    </div>
-                    <div className="flex gap-2 items-center">
-                        <MapPin />
-                        <p>Uspallata 2120, Hurlingham</p>
-                    </div>
-                </div>
-                <a href={BOOKING_URL} className="text-zinc-100 text-2xl px-3 py-0.5 bg-violet-400 rounded-full text-center ">RESERVA TU TURNO!</a>
             </div>
-
-
-
+            <div>
+                <h1>© 2026 Haka Nails</h1>
+                <p>Desarrollado por Martin Peralta</p>
+            </div>
         </section>
 
     );
